a247a5d0afc4a7ec7aa1dcda3c5a048c7618eba6
abdelrauof mohammed <abdelrauof97@gmail.com>
2018-01-09 20:18:59 +0000

    automatically backed up by learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,16 @@
 require 'pry'
 
 def my_find(collection)
-
+  i = 0 
+  new_array = []
+  while i < collection.length 
+    if yield(collection[i])
+      return collection[i]
+    end
+    i += 1
+  end
+  new_array
 end
+collection = (1..100).to_a
+binding.pry
+my_find(collection) { |x| x % 3 == 0  and x % 5 == 0}
